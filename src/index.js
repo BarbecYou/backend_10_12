1fec9c83e1ce2168d8986096048d64e8c6a9f094
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-13 13:00:19 +0200

    email validalasa kesz

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@
 // npx webpack watch
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('submitButton').addEventListener('click', () => {
-        let elem = document.getElementById('usernameInput');
-        let felhasznaloNev = elem.value;
-        if (felhasznaloNev.length < 6 || felhasznaloNev.length > 30 || felhasznaloNev.includes('&') || felhasznaloNev.includes('=') || felhasznaloNev.includes('_') || felhasznaloNev.includes('-') || felhasznaloNev.includes('+') || /* felhasznaloNev.includes('\,') || felhasznaloNev.includes('\'')*/ felhasznaloNev.includes('<') || felhasznaloNev.includes('>') || (felhasznaloNev.match('.').length > 1)) {
-            console.log("hibas nev");
+        let felhasznaloNev = document.getElementById('usernameInput').value;
+        let email = document.getElementById('emailInput').value;
+        if (felhasznaloNev.length < 6 || felhasznaloNev.length > 30 || felhasznaloNev.includes('&') || felhasznaloNev.includes('=') || felhasznaloNev.includes('_') || felhasznaloNev.includes('-') || felhasznaloNev.includes('+') || felhasznaloNev.includes('\,') || felhasznaloNev.includes('\'') || felhasznaloNev.includes('<') || felhasznaloNev.includes('>') || (((felhasznaloNev.split('.').length - 1) > 1) || (felhasznaloNev[0] == '.'))) {
+            window.alert("Hibás felhasználónév!");
+        }
+        if (!(email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))) {
+            window.alert("Hibás e-mail cím!");
+        }
+        else {
+            window.alert("err!");
         }
     });
 });
+const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return String(email)
+        .toLowerCase()
+        .match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+};
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,30 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('submitButton')!.addEventListener('click', () => {
-        let elem : HTMLInputElement= document.getElementById('usernameInput') as HTMLInputElement;
-        let felhasznaloNev : string = elem.value;
+        let felhasznaloNev : string = (document.getElementById('usernameInput') as HTMLInputElement).value;
+        let email : string = (document.getElementById('emailInput') as HTMLInputElement).value;
+
         
-        if (felhasznaloNev.length < 6 || felhasznaloNev.length > 30 || felhasznaloNev.includes('&') || felhasznaloNev.includes('=') || felhasznaloNev.includes('_') || felhasznaloNev.includes('-') || felhasznaloNev.includes('+') || /* felhasznaloNev.includes('\,') || felhasznaloNev.includes('\'')*/ felhasznaloNev.includes('<') || felhasznaloNev.includes('>') || (felhasznaloNev.match('.')!.length > 1)){
-            console.log("hibas nev");
+        if (felhasznaloNev.length < 6 || felhasznaloNev.length > 30 || felhasznaloNev.includes('&') || felhasznaloNev.includes('=') || felhasznaloNev.includes('_') || felhasznaloNev.includes('-') || felhasznaloNev.includes('+') || felhasznaloNev.includes('\,') || felhasznaloNev.includes('\'') || felhasznaloNev.includes('<') || felhasznaloNev.includes('>') || (((felhasznaloNev.split('.').length - 1) > 1) || (felhasznaloNev[0] == '.'))){
+            window.alert("Hibás felhasználónév!");
+        }
+        if (!(email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          ))){
+            window.alert("Hibás e-mail cím!");
+        }else {
+            window.alert("err!");
         }
          
     });
 });
+
+const validateEmail = (email : string) => {
+    const re : RegExp =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+      );
+  };
